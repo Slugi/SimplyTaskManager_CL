7016b821e69392ea522c6ada81f2cc3620aa3e58
Slugi <slugii@hotmail.com>
2020-10-10 14:47:17 +0200

    Dodane funkcje add i remove(bez zapisu)

--- src/pl/coderslab/TaskManager.java
+++ src/pl/coderslab/TaskManager.java
@@ -1,5 +1,8 @@
 package pl.coderslab;
 
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -12,12 +15,12 @@ import java.util.Scanner;
 public class TaskManager {
   static final String FILE_NAME = "tasks.csv";
   static final String[] OPTIONS = {"add", "remove", "list", "exit"};
-  static String[][] Tasks;
+  static String[][] zadania;
 
   public static void main(String[] args) throws IOException {
-    Tasks = odczyt(FILE_NAME);
+    zadania = odczyt(FILE_NAME);
     odczyt(FILE_NAME);
-    chooseOption(OPTIONS);
+    wybierzOpcje(OPTIONS);
     menu(OPTIONS);
   }
 
@@ -44,14 +47,21 @@ public class TaskManager {
     return tab;
   }
 
-  public static void chooseOption(String[] tab) {
+  public static void wybierzOpcje(String[] tab) {
     menu(OPTIONS);
     Scanner scan = new Scanner(System.in);
     while (scan.hasNextLine()) {
       String input = scan.nextLine();
       switch (input) {
         case "list":
-          pokazListe(Tasks);
+          pokazListe(zadania);
+          break;
+        case "remove":
+          usuwanieZadania(zadania,podajNumer());
+          System.out.println("Zadanie zostało pomyślnie usunięte.");
+          break;
+        case "add":
+          dodawanieZadania();
           break;
         case "exit":
           System.out.println(ConsoleColors.RED);
@@ -80,5 +90,47 @@ public class TaskManager {
     for (String option : tab) {
       System.out.println(option);
     }
+  }
+
+  public static boolean czyWiekszaRownaZero(String input) {
+    if (NumberUtils.isParsable(input)) {
+      return Integer.parseInt(input) >= 0;
+    }
+    return false;
+  }
+  public static int podajNumer(){
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Podaj numer zadania do usunięcia");
+    String num = scanner.nextLine();
+    while(!czyWiekszaRownaZero(num)){
+      System.out.println("Nieprawidłowa watrość. Podaj numer większy lub równy 0");
+      scanner.nextLine();
+    }
+
+    return Integer.parseInt(num);
+  }
+  private static void usuwanieZadania(String[][] tab, int index){
+    try{
+      if(index<tab.length){
+        zadania = ArrayUtils.remove(tab,index);
+      }
+    } catch (ArrayIndexOutOfBoundsException ex) {
+      System.err.println("Podany element nie występuje na liście.");
+    }
+  }
+  private static void dodawanieZadania(){
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Podaj opis zadania.");
+    String opis = scanner.nextLine();
+    System.out.println("Podaj termin wykonania zadania.");
+    String data = scanner.nextLine();
+    System.out.println("Czy zadanie jest ważne: true/false");
+    String czyWazne = scanner.nextLine();
+    zadania = Arrays.copyOf(zadania, zadania.length+1);
+    zadania[zadania.length-1] = new String[3];
+    zadania[zadania.length-1][0] = opis;
+    zadania[zadania.length-1][1] = data;
+    zadania[zadania.length-1][2] = czyWazne;
+
   }
 }
